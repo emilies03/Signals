7369ea6b294ac411eb9ff3f20d8ffc9f7dd77fb8
Cameron Gibb <cameronrgibb@gmail.com>
2020-11-16 11:40:57 +0000

    Produces Hilbert Filter Coefficients as an array of floats

--- src/hilbert_filter.c
+++ src/hilbert_filter.c
@@ -1,16 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main(){
 
-    int i;
+    //Sets order of filter
     int filter_order = 40;
     int numbers[filter_order+1];
+    int i;
 
+    //This array contains the final hilbert filter coefficients
+    float hilbert_filter_coefficients[filter_order+1];
+    
+ 
+
+    //Loop puts order numbers in each array slot
     for(i=0; i<=filter_order; i++){
         numbers[i] = i - filter_order/2;
-        printf("%d", numbers[i]);
-        printf("\n");
+    }
+
+    //Places zeros in even slots and calculates Hilbert coefficient in odd slots
+    for(i=0; i<= filter_order; i++){
+        if(numbers[i] % 2 == 0){
+            hilbert_filter_coefficients[i] = 0;
+        }
+        else{
+            hilbert_filter_coefficients[i] = 2/(numbers[i]*M_PI);
+        }
+
+        //uncomment to print filter coefficients to terminal
+
+        // printf("%f", hilbert_filter_coefficients[i]);
+        // printf("\n");
     }
 
     exit(0);
