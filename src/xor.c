9b676f9e37afd36b70bda248390c0333f0af4a38
Charlie <charlesthomash@virginmedia.com>
2020-12-02 19:52:03 +0000

    xor functino test

--- src/xor.c
+++ src/xor.c
@@ -3,75 +3,125 @@
 #include<math.h>
 
 unsigned long long bin_to_uint64();
-float uint64_to_bin();
+double uint64_to_bin();
 
-int xor(unsigned long long* prs1, unsigned long long* prs2){
+int xor(unsigned long long prs1, unsigned long long prs2, int in_array[128]){
+    int i, final_bit;
     float average_bit;
 
-    int prs_test;
-    int input_test[16] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0};
-    int xored_prs_input;
+    unsigned int prs_test = 2;
+    int input_test[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0};
+    unsigned int xored_prs_input;
     unsigned long long input_test_single;
     int array1[64], array2[64];
+    unsigned long long num1, num2;
+    unsigned long long signal1_xored_prs1, signal2_xored_prs2;
+    double average_bit_1, average_bit_2;
 
+
+    //checking prs read in
+
+    //prs1 = 1;
+    //prs2 = 0;
+    printf("\n");
+    // split 128 bit in_array into 2, 64bit arrays
     for (i=0; i<128; i++){
         if(i<64){
-            array1[64-i] = in_array[128-i]
+            array1[63-i] = in_array[127-i];
+            printf(" %i,", array1[63-i]);
         }
         else{
-            array2[64-i] = in_array[128-i]
+            array2[63-i+64] = in_array[127-i];
+            printf("| %i ", array2[63-i+64]);
         }
 
     }
 
-    printf("about to convert input from binary to base 10\n");
-    input_test_single = bin_to_uint64(array);
+    // convert binary 64bit arrays into uint64
+    num1 = bin_to_uint64(array1);
+    num2 = bin_to_uint64(array2);
 
-    prs_test = 1;
+    // Xor signals with corresponding PRS
+    signal1_xored_prs1 = prs1^num1;
+    signal2_xored_prs2 = prs2^num2;
 
-    xored_prs_input = prs_test^input_test_single;
+    printf("signal1_xored_prs1 = %llu\n",signal1_xored_prs1);
+    printf("signal2_xored_prs2 = %llu\n",signal2_xored_prs2);
 
-    average_bit = uint64_to_bin(xored_prs_input);
+    // find average binary number of xored signals
+    average_bit_1 = uint64_to_bin(signal1_xored_prs1);
+    average_bit_2 = uint64_to_bin(signal2_xored_prs2);
+
+    printf("average bit 1 = %f\n", average_bit_1);
+    printf("average bit 2 = %f\n", average_bit_2);
+
+    // assign bit based on average bit (average_bit > 0.5 = 1; average_bit < 0.5 =0)
+    if(((average_bit_1+average_bit_2)/2) >= 0.5){
+        final_bit = 1;
+    }
+
+    if(((average_bit_1+average_bit_2)/2) < 0.5){
+        final_bit = 0;
+    }
+
+
+    
+
+    // printf("about to convert input from binary to base 10\n");
+    // input_test_single = bin_to_uint64(input_test);
+    // printf("input test signal = %lu\n", input_test_single);
+
+    // prs_test = 0;
+
+    // xored_prs_input = prs_test^input_test_single;
+    // printf("input test single: %llu\n", input_test_single);
+
+    // printf("prs: %d\n", prs_test);
+    // printf("xored_prs_input: %d\n", xored_prs_input);
+    // average_bit = uint64_to_bin(xored_prs_input);
+    // printf("average bit = %f\n",average_bit);
+    // printf("prs_test = %i\n", prs_test);
 
 
 
-    printf("input test single: %llu\n", input_test_single);
-
-    printf("prs: %d\n", prs_test);
-    printf("xored_prs_input: %d\n", xored_prs_input);
-    printf("average bit = %f\n",average_bit);
-    printf("prs = %llu\n", prs);
-
-
-
-return 1;
+return final_bit;
 }
 
-float uint64_to_bin(int xored_prs_input){
+double uint64_to_bin(unsigned long long xored_prs_input){
     int i, remainder, sum = 0;
-    int array_bin[10];
-    for (i = 0; i < 11; i++){
+    int array_bin[64];
+    for (i = 0; i < 64; i++){
+        printf("xored prs input = %llu\n",xored_prs_input);
         remainder = xored_prs_input%2;
         sum += remainder;
-        array_bin[i-10] = remainder;
-        if(xored_prs_input%2 == 0){
+        array_bin[i-63] = remainder;
+        if(remainder == 0){
             xored_prs_input = xored_prs_input/2;
         }
-        else{
-            xored_prs_input = xored_prs_input/2 - 0.5;
+        else if (remainder == 1){
+            xored_prs_input = xored_prs_input/2;
         }
-        printf("%i\n", array_bin[i-10]);
+        printf("%i = %i\n", i,remainder);
     }
-    return (double)sum/11;
+    // for (i = 0; i < 64; i++){
+    //     printf("%i",array_bin[i]);
+    // }
+    return (double)sum/64;
 }
 
-unsigned long long bin_to_uint64(int binary_array[10]){
-    int i, n = 0;
+unsigned long long bin_to_uint64(int binary_array[64]){
+    int i = 0;
+    unsigned long long n = 0;
     unsigned long long sum = 0;
-    for (i = 0; i <= 9; i++) {
+    for (i = 0; i < 64; i++) {
         n = pow(2,(double)i);
-        sum += (n * binary_array[9-i]);
+        printf(" n = %llu ",n);
+        printf(" binarray_array %i = %i ", i,binary_array[63-i]);
+        sum += (n * binary_array[63-i]);
+        printf("%llu",n * binary_array[63-i]);
+        printf("\n");
     }
+    printf("bin to unit64 in the conversion function = %llu\n",sum);
     return sum;
 }
 
